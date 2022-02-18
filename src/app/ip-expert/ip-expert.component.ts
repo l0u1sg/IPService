528a71482bb94a1bf6e31a0178b1eade32e45518
Louis Gallet <louis.gallet@tumo.world>
2022-02-19 00:20:09 +0100

    feat: :construction:  starting api request

--- src/app/ip-expert/ip-expert.component.ts
+++ src/app/ip-expert/ip-expert.component.ts
@@ -1,13 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError} from 'rxjs/operators';
+
+export interface IpAddress {
+  // location
+  lat: number;
+  lon: number;
+  location: string;
+  city: string;
+  zip: string;
+  language: string;
+  timezone: string;
+
+  // connection
+  ip: string;
+  isp: string;
+  type: string;
+  ASN: string;
+  hostname: string;
+
+  // security
+  proxy: boolean;
+  tor: boolean;
+  crawler: boolean;
+  threat: boolean;
+
+  // other
+  loading: boolean;
+  errorMessage: string;
+}
+
 
 @Component({
   selector: 'app-ip-expert',
   templateUrl: './ip-expert.component.html',
   styleUrls: ['./ip-expert.component.css']
 })
+@Injectable()
 export class IpExpertComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
